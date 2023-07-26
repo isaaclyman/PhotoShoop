c98f4f1c91795cf5840e70f3fcb7e0cd7d4abf1d
Isaac Lyman <github@isaaclyman.com>
2023-07-25 19:40:23 -0600

    Fix style and functionality bugs

--- src/web/slides.js
+++ src/web/slides.js
@@ -1,28 +1,38 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+let nextPhotoTimeout;
+
+function imageError() {
+  getNextPhoto();
+}
+
+function getNextPhoto() {
   const viewpane = document.querySelector(".viewpane");
   const root = window.location.host;
   const photoUrl = 'http://' + root + "/next";
 
-  function getNextPhoto() {
-    fetch(photoUrl)
-      .then((res) => res.blob())
-      .then((blob) =>
-        blob.type.startsWith("image/heic") ? heic2any({ blob }) : blob
-      )
-      .then((blob) => {
-        const image = URL.createObjectURL(blob);
-        viewpane.classList.add("fade-out");
-
-        setTimeout(() => {
-          viewpane.src = image;
-          setTimeout(() => {
-            viewpane.classList.remove("fade-out");
-            setTimeout(() => getNextPhoto(), 4000);
-          }, 250);
-        }, 300);
-      });
+  if (nextPhotoTimeout) {
+    clearTimeout(nextPhotoTimeout);
   }
 
+  fetch(photoUrl)
+    .then((res) => res.blob())
+    .then((blob) =>
+      blob.type.startsWith("image/heic") ? heic2any({ blob }) : blob
+    )
+    .then((blob) => {
+      const image = URL.createObjectURL(blob);
+      viewpane.classList.add("fade-out");
+
+      setTimeout(() => {
+        viewpane.src = image;
+        setTimeout(() => {
+          viewpane.classList.remove("fade-out");
+          nextPhotoTimeout = setTimeout(() => getNextPhoto(), 4000);
+        }, 250);
+      }, 300);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
   getNextPhoto();
 
   setTimeout(() => {
